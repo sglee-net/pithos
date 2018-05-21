f95492a9bcecd57b97127e14fef4102d0096406e
sglee <sg10.lee@gmail.com>
2018-05-21 17:36:54 +0900

    initial commit

--- src/main/java/org/chronotics/pithos/Application.java
+++ src/main/java/org/chronotics/pithos/Application.java
@@ -1,6 +1,5 @@
 package org.chronotics.pithos;
 
-import org.chronotics.db.mybatis.app.service.ServiceUser;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -8,19 +7,12 @@ import org.springframework.context.annotation.ComponentScan;
 
 @ComponentScan(basePackages = {
 		"org.chronotics.pithos",
-		"org.chronotics.db.mybatis",
-		"org.chronotics.db.mybatis.app.controller",
-		"org.chronotics.db.mybatis.app.mapper",
-		"org.chronotics.db.mybatis.app.service"})
+		"org.chronotics.db.mybatis"})
 @SpringBootApplication
-public class Application { //extends RepositoryRestMvcConfiguration {
+public class Application {
 	
 	public static void main(String[] args) {
-
 		ApplicationContext context = SpringApplication.run(Application.class,args);
-
-		ServiceUser service = (ServiceUser)context.getBean("serviceUser");
-		service.selectListByName("testUser");
 	}
 
 }
