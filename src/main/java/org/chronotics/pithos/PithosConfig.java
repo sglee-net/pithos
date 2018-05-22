de0847617f034295f71207d90253a836ea993e5e
sglee <sg10.lee@gmail.com>
2018-05-22 11:09:32 +0900

    change Config to PithosConfig

--- src/main/java/org/chronotics/pithos/PithosConfig.java
+++ src/main/java/org/chronotics/pithos/PithosConfig.java
@@ -12,6 +12,6 @@ import org.springframework.context.annotation.ImportResource;
  */
 @Configuration
 @ImportResource("classpath:applicationContext.xml")
-public class Config {
+public class PithosConfig {
 
 }
